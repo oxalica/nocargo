c75c6933b555477ae84b6e59a355c95cb0513c33
oxalica <oxalicc@pm.me>
2021-08-20 19:32:21 +0800

    Handle difference sources in `nocargo init`

--- nocargo/src/init.rs
+++ nocargo/src/init.rs
@@ -1,9 +1,12 @@
+use std::collections::BTreeSet;
 use std::io::Write;
 use std::path::{Path, PathBuf};
 
 use anyhow::{bail, ensure, Context, Result};
 use askama::Template;
 use cargo_toml::{Dependency, Manifest};
+use once_cell::sync::Lazy;
+use regex::Regex;
 use structopt::StructOpt;
 
 use crate::App;
@@ -24,13 +27,21 @@ pub struct Opt {
 }
 
 // https://doc.rust-lang.org/cargo/reference/specifying-dependencies.html
-// TODO: Support more sources.
-#[allow(dead_code)]
+#[derive(Debug, Clone, Copy)]
 enum DepSource<'a> {
     CratesIo,
-    ExternalRegistry { registry: &'a str },
+    RegistryName { name: &'a str },
+    RegistryUrl { url: &'a str, },
     Path { path: &'a Path },
-    Git { url: &'a str, rev: Option<&'a str> },
+    Git { url: &'a str, ref_: GitRef<'a> },
+}
+
+#[derive(Debug, Clone, Copy)]
+enum GitRef<'a> {
+    NotSpecified,
+    Tag(&'a str),
+    Branch(&'a str),
+    Rev(&'a str),
 }
 
 impl<'a> DepSource<'a> {
@@ -38,24 +49,26 @@ impl<'a> DepSource<'a> {
         match dep {
             Dependency::Simple(_) => Ok(Self::CratesIo),
             Dependency::Detailed(detail) => {
-                match (&detail.registry, &detail.path, &detail.git) {
-                    (Some(registry), None, None) => Ok(Self::ExternalRegistry { registry }),
-                    (None, Some(path), None) => Ok(Self::Path {
+                match (&detail.registry, &detail.registry_index, &detail.path, &detail.git) {
+                    (Some(name), None, None, None) => Ok(Self::RegistryName { name }),
+                    (None, Some(url), None, None) => Ok(Self::RegistryUrl { url }),
+                    (None, None, Some(path), None) => Ok(Self::Path {
                         path: Path::new(path),
                     }),
-                    (None, None, Some(url)) => {
-                        let tot = detail.branch.is_some() as u32
-                            + detail.rev.is_some() as u32
-                            + detail.tag.is_some() as u32;
-                        ensure!(tot <= 1, "For git dependency, at most one of `branch`, `rev` and `tag` is allowed");
-                        let rev = detail
-                            .branch
-                            .as_deref()
-                            .or(detail.rev.as_deref())
-                            .or(detail.tag.as_deref());
-                        Ok(Self::Git { url, rev })
+                    (None, None, None, Some(url)) => {
+                        let ref_ = match (&detail.branch, &detail.tag, &detail.rev) {
+                            (None, None, None) => GitRef::NotSpecified,
+                            (Some(b), None, None) => GitRef::Branch(b),
+                            (None, Some(t), None) => GitRef::Tag(t),
+                            (None, None, Some(r)) => GitRef::Rev(r),
+                            _ => bail!("For git dependency, at most one of `branch`, `rev` and `tag` is allowed"),
+                        };
+                        Ok(Self::Git { url, ref_ })
                     }
-                    _ => bail!("Ambiguous dependency source: {:?}", dep),
+                    _ => bail!(
+                        "Only one of `registry`, `registry-index`, `path`, `git` can be specified: {:?}",
+                        dep,
+                    ),
                 }
             }
         }
@@ -94,7 +107,7 @@ impl App for Opt {
     }
 }
 
-fn generate_flake(manifest: &Manifest) -> Result<String> {
+pub fn generate_flake(manifest: &Manifest) -> Result<String> {
     ensure!(manifest.patch.is_empty(), "[patch] is not supported yet");
     ensure!(
         manifest.workspace.is_none(),
@@ -102,7 +115,7 @@ fn generate_flake(manifest: &Manifest) -> Result<String> {
     );
 
     let crate_name = &manifest.package.as_ref().context("Missing [package]")?.name;
-    let templ = FlakeNixTemplate::new(crate_name.clone());
+    let mut templ = FlakeNixTemplate::new(crate_name.clone());
 
     let deps = manifest
         .dependencies
@@ -122,12 +135,33 @@ fn generate_flake(manifest: &Manifest) -> Result<String> {
         match source {
             // Automatically handled.
             DepSource::CratesIo => {}
-            DepSource::ExternalRegistry { .. } => {
-                bail!("External registry is not supported yet for {:?}", dep_name)
+            DepSource::RegistryName { .. } => {
+                bail!("External registry with name is not supported yet for {:?}", dep_name)
             }
-            DepSource::Path { .. } => bail!("Local path is not supported yet for {:?}", dep_name),
-            DepSource::Git { .. } => {
-                bail!("Git dependency is not supported yet for {:?}", &dep_name)
+            DepSource::Path { .. } => {
+                bail!("Local path dependency is not supported yet for {:?}", dep_name)
+            }
+            DepSource::RegistryUrl { url } => {
+                let flake_ref = git_url_to_flake_ref(url, "master", None)?;
+                templ.add_registry(url.to_owned(), flake_ref)?;
+            }
+            DepSource::Git { url, ref_ } => {
+                let (ref_name, rev) = match ref_ {
+                    GitRef::Tag(ref_name) | GitRef::Branch(ref_name) => (ref_name, None),
+                    // FIXME: Need a warning if ref is missing.
+                    GitRef::NotSpecified => ("master", None),
+                    GitRef::Rev(rev) => ("master", Some(rev)),
+                };
+                let flake_ref = git_url_to_flake_ref(url, ref_name, rev)?;
+
+                let source_url = match ref_ {
+                    GitRef::NotSpecified => url.to_owned(),
+                    GitRef::Tag(tag) => format!("{}?tag={}", url, tag),
+                    GitRef::Branch(branch) => format!("{}?branch={}", url, branch),
+                    GitRef::Rev(rev) => format!("{}?rev={}", url, rev),
+                };
+
+                templ.add_git_src(source_url, flake_ref)?;
             }
         }
     }
@@ -140,14 +174,96 @@ fn generate_flake(manifest: &Manifest) -> Result<String> {
 #[template(path = "flake.nix", escape = "none")]
 struct FlakeNixTemplate {
     crate_name: String,
-    crate_name_escaped: String,
+    // (source_id, flake_ref)
+    registries: BTreeSet<(String, String)>,
+    // (source_id, flake_ref)
+    git_srcs: BTreeSet<(String, String)>,
+}
+
+// https://djc.github.io/askama/filters.html
+mod filters {
+    pub fn nix_str(s: &str) -> askama::Result<String> {
+        Ok(s.escape_default().to_string())
+    }
 }
 
 impl FlakeNixTemplate {
     fn new(crate_name: String) -> Self {
         Self {
-            crate_name_escaped: format!("{:?}", crate_name),
             crate_name,
+            registries: BTreeSet::new(),
+            git_srcs: BTreeSet::new(),
         }
+    }
+
+    fn add_registry(&mut self, source_id: String, flake_url: String) -> Result<()> {
+        self.registries.insert((source_id, flake_url));
+        Ok(())
+    }
+
+    fn add_git_src(&mut self, source_id: String, flake_url: String) -> Result<()> {
+        self.git_srcs.insert((source_id, flake_url));
+        Ok(())
+    }
+}
+
+// https://nixos.org/manual/nix/unstable/command-ref/new-cli/nix3-flake.html?#flake-inputs
+pub fn git_url_to_flake_ref(url_orig: &str, ref_name: &str, rev: Option<&str>) -> Result<String> {
+    let url = url_orig.strip_prefix("git+").unwrap_or(url_orig);
+    ensure!(
+        url.starts_with("http://") ||
+        url.starts_with("https://") ||
+        url.starts_with("ssh://") ||
+        url.starts_with("git://"),
+        "Only http/https/ssh/git schema are supported for git url: {:?}",
+        url_orig,
+    );
+
+    static RE_GITHUB_URL: Lazy<Regex> = Lazy::new(|| {
+        Regex::new(r"^https?://github.com/([^/?#]+)/([^/?#]+?)(.git)?/?$").unwrap()
+    });
+
+    if let Some(cap) = RE_GITHUB_URL.captures(url) {
+        let owner = cap.get(1).unwrap().as_str();
+        let repo = cap.get(2).unwrap().as_str();
+        let rev = rev.unwrap_or(ref_name);
+        return Ok(format!("github:{}/{}/{}", owner, repo, rev));
+    }
+
+    ensure!(
+        !url.contains(|c| c == '?' || c == '#'),
+        "Url containing `?` or `#` is not supported yet: {:?}",
+        url_orig,
+    );
+
+    let prefix = if url.starts_with("git://") { "" } else { "git+" };
+
+    match rev {
+        None => Ok(format!("{}{}?ref={}", prefix, url, ref_name)),
+        Some(rev) => Ok(format!("{}{}?ref={}&rev={}", prefix, url, ref_name, rev)),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_flake_url() {
+        let f = git_url_to_flake_ref;
+        // Schemas.
+        assert_eq!(f("https://example.com", "ref", None).unwrap(), "git+https://example.com?ref=ref");
+        assert_eq!(f("https://example.com", "ref", Some("123")).unwrap(), "git+https://example.com?ref=ref&rev=123");
+        assert_eq!(f("git://example.com", "ref", None).unwrap(), "git://example.com?ref=ref");
+        assert_eq!(f("git+git://example.com", "ref", None).unwrap(), "git://example.com?ref=ref");
+        assert_eq!(f("git+ssh://git@github.com/foo/bar", "ref", None).unwrap(), "git+ssh://git@github.com/foo/bar?ref=ref");
+
+        // GitHub.
+        assert_eq!(f("https://github.com/foo/bar", "ref", None).unwrap(), "github:foo/bar/ref");
+        assert_eq!(f("https://github.com/foo/bar", "ref", Some("123")).unwrap(), "github:foo/bar/123");
+        assert_eq!(f("https://github.com/foo/bar.git", "ref", Some("123")).unwrap(), "github:foo/bar/123");
+        assert_eq!(f("https://github.com/foo/bar/", "ref", Some("123")).unwrap(), "github:foo/bar/123");
+        assert_eq!(f("http://github.com/foo/bar.git", "ref", Some("123")).unwrap(), "github:foo/bar/123");
+        assert_eq!(f("git+https://github.com/foo/bar.git", "ref", Some("123")).unwrap(), "github:foo/bar/123");
     }
 }
--- nocargo/templates/flake.nix
+++ nocargo/templates/flake.nix
@@ -1,11 +1,27 @@
 {
-  description = "Rust crate {{ crate_name }}";
+  description = "Rust crate {{ crate_name|nix_str }}";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    crates-io-index = { url = "github:rust-lang/crates.io-index"; flake = false; };
-    nocargo = { url = "github:oxalica/nocargo"; inputs.crates-io-index.follows = "crates-io-index"; };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    nocargo = {
+      url = "github:oxalica/nocargo";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.registry-crates-io.follows = "registry-crates-io";
+    };
+
+    registry-crates-io = { url = "github:rust-lang/crates.io-index"; flake = false; };
+    {%- for (_, flake_ref) in registries %}
+    registry-{{ loop.index }} = { url = "{{ flake_ref|nix_str }}"; flake = false; };
+    {%- endfor %}
+    {%- for (_, flake_ref) in git_srcs %}
+    git-{{ loop.index }} = { url = "{{ flake_ref|nix_str }}"; flake = false; };
+    {%- endfor %}
   };
 
   outputs = { nixpkgs, flake-utils, rust-overlay, nocargo, ... }@inputs:
@@ -13,16 +29,35 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [rust-overlay.overlay nocargo.overlay ];
+          overlays = [ rust-overlay.overlay nocargo.overlay ];
         };
+
         rustc = pkgs.rust-bin.stable.latest.minimal;
+        {%- if !registries.is_empty() %}
+        extraRegistries = {
+          {%- for (source_id, _) in registries %}
+          "{{ source_id|nix_str }}" = pkgs.lib.nocargo.mkIndex inputs.registry-{{ loop.index }};
+          {%- endfor %}
+        };
+        {%- endif %}
+        {%- if !git_srcs.is_empty() %}
+        gitSources = {
+          {%- for (source_id, _) in git_srcs %}
+          "{{ source_id|nix_str }}" = inputs.git-{{ loop.index }};
+          {%- endfor %}
+        };
+        {%- endif %}
+
       in
       rec {
-        defaultPackage = packages.{{ crate_name_escaped }};
+        defaultPackage = packages."{{ crate_name|nix_str }}";
         defaultApp = defaultPackage.bin;
-        packages.{{ crate_name_escaped }} = pkgs.nocargo.buildRustCrateFromSrcAndLock {
-          # inherit rustc;
+
+        packages."{{ crate_name|nix_str }}" = pkgs.nocargo.buildRustCrateFromSrcAndLock {
           src = ./.;
+          inherit /* rustc */
+            {%- if !registries.is_empty() %} extraRegistries{% endif %}
+            {%- if !git_srcs.is_empty() %} gitSources{% endif %};
         };
       });
 }
