ef3d23eb26d2bdd5de7d6596fff21cd09287b23f
oxalica <oxalicc@pm.me>
2022-07-14 01:13:50 +0800

    cratesio-utils: refactor and optimize

--- scripts/cratesio-utils.py
+++ scripts/cratesio-utils.py
@@ -2,14 +2,15 @@
 #!nix-shell -i python3 -p cargo "python3.withPackages (ps: with ps; [ aiohttp toml ])"
 from io import BytesIO
 from pathlib import Path
-from typing import Optional
+from typing import Callable, Optional
+from typing_extensions import Self
 from urllib import request
 import aiohttp
 import argparse
 import asyncio
 import csv
+import os
 import re
-import shutil
 import sqlite3
 import subprocess
 import sys
@@ -25,9 +26,8 @@ CRATE_TARBALL_URL = 'https://static.crates.io/crates/{name}/{name}-{version}.cra
 POPULAR_CRATES_MANIFEST_PATH = Path(__file__).parent.parent / 'cache' / 'Cargo.toml'
 PROC_MACRO_LIST_PATH = Path(__file__).parent.parent / 'crates-io-override' / 'proc-macro.nix'
 
-# nix-shell would mess up with `TMPDIR`.
-TMP_DB_DIR = Path('/tmp/cratesio-db-dump')
-TMP_CRATES_TOML_DIR = Path('/tmp/cratesio-toml')
+CACHE_DIR = Path(os.environ.get('XDG_CACHE_HOME') or (Path.home() / '.cache')) / 'cratesio'
+CRATES_TOML_DIR = CACHE_DIR / 'toml'
 
 def noisily(*args) -> None:
     print(*args, file=sys.stderr)
@@ -35,111 +35,139 @@ def noisily(*args) -> None:
 class CratesioDB(sqlite3.Connection):
     DB_URL = 'https://static.crates.io/db-dump.tar.gz'
 
-    DB_DIR = Path('/tmp/cratesio-db-dump') # nix-shell would mess up with `TMPDIR`.
+    DB_DIR = CACHE_DIR
     DB_PATH = DB_DIR / 'db.sqlite'
     MTIME_PATH = DB_DIR / 'mtime.txt'
 
     INIT_SQL = r'''
-        pragma journal_mode=off;
-        create table crates(
-            created_at text,
-            description text,
-            documentation text,
-            downloads text,
-            homepage text,
-            id int not null primary key,
-            max_update_size int,
-            name text,
-            readme text,
-            repository text,
-            updated_at text
-        );
-        create table versions(
-            crate_id int not null,
-            crate_size int,
-            created_at text,
-            downloads int,
-            features text,
-            id int not null primary key,
-            license text,
-            num text,
-            published_by text,
-            updated_at text,
-            yanked text
-        );
-        create table version_downloads(
-            date text,
-            downloads int,
-            version_id int not null
-        );
+        PRAGMA journal_mode = off;
+        PRAGMA cache_size = -{cache_kb};
+
+        CREATE TABLE IF NOT EXISTS crates (
+            id INTEGER NOT NULL PRIMARY KEY,
+            name TEXT NOT NULL,
+            created_at TEXT NOT NULL,
+            updated_at TEXT NOT NULL,
+            downloads INTEGER NOT NULL
+        ) STRICT;
+        CREATE TABLE IF NOT EXISTS versions (
+            id INTEGER NOT NULL PRIMARY KEY,
+            crate_id INTEGER NOT NULL,
+            num TEXT NOT NULL,
+            updated_at TEXT NOT NULL,
+            created_at TEXT NOT NULL,
+            downloads INTEGER NOT NULL,
+            features TEXT NOT NULL,
+            yanked INTEGER NOT NULL,
+            license TEXT
+        ) STRICT;
+        CREATE TABLE IF NOT EXISTS version_downloads (
+            version_id INTEGER NOT NULL,
+            downloads INTEGER NOT NULL,
+            date TEXT NOT NULL,
+            PRIMARY KEY (version_id, date)
+        ) STRICT, WITHOUT ROWID;
     '''
-    INSERT_SQLS = {
-        'crates': 'insert into crates values (?,?,?,?,?,?,?,?,?,?,?)',
-        'versions': 'insert into versions values (?,?,?,?,?,?,?,?,?,?,?)',
-        'version_downloads': 'insert into version_downloads values (?,?,?)',
+
+    INIT_INDEX_SQL = r'''
+        CREATE INDEX IF NOT EXISTS versions_ix_crate_to_version ON versions
+            (crate_id, id);
+        ANALYZE;
+    '''
+
+    INSERTERS: dict[str, tuple[str, Callable[[dict[str, str]], tuple]]] = {
+        'crates.csv': (
+            'INSERT INTO crates VALUES (?,?,?,?,?)',
+            lambda row: (
+                int(row['id']),
+                row['name'],
+                row['created_at'],
+                row['updated_at'],
+                int(row['downloads']),
+            ),
+        ),
+        'versions.csv': (
+            'INSERT INTO versions VALUES (?,?,?,?,?,?,?,?,?)',
+            lambda row: (
+                int(row['id']),
+                int(row['crate_id']),
+                row['num'],
+                row['updated_at'],
+                row['created_at'],
+                int(row['downloads']),
+                row['features'],
+                row['yanked'] == 't',
+                row['license'],
+            ),
+        ),
+        'version_downloads.csv': (
+            'INSERT INTO version_downloads VALUES (?,?,?)',
+            lambda row: (
+                int(row['version_id']),
+                int(row['downloads']),
+                row['date'],
+            )
+        ),
     }
 
-    def __init__(self, sync: bool=False) -> None:
-        if sync or not self.DB_PATH.exists():
-            self._sync_and_open()
-        else:
-            self._open()
-
-    def _open(self) -> None:
+    def __init__(self, *, check: bool=True, cache_mb: int=1024) -> None:
+        assert not check or self.MTIME_PATH.exists(), 'Database not initialized, please run `sync` subcommand'
         super().__init__(str(self.DB_PATH))
+        self.executescript(self.INIT_SQL.format(cache_kb=cache_mb * 1024))
 
-    def _sync_and_open(self) -> None:
-        self.DB_DIR.mkdir(exist_ok=True)
-        last_mtime = self.MTIME_PATH.read_text() if self.MTIME_PATH.is_file() else None
+    @classmethod
+    def sync(cls, *, cache_mb: int=1024) -> None:
+        cls.DB_DIR.mkdir(exist_ok=True)
+        last_mtime = cls.MTIME_PATH.read_text() if cls.MTIME_PATH.exists() else None
 
-        noisily('Syncing database')
-        with request.urlopen(self.DB_URL) as resp:
+        noisily('Synchronizing database')
+        with request.urlopen(cls.DB_URL) as resp:
             assert resp.status == 200, f'HTTP failure {resp.status}'
             mtime: str = resp.headers['last-modified']
-            if mtime == last_mtime and self.DB_PATH.exists():
+            if mtime == last_mtime:
                 noisily(f'Database is up-to-date at {mtime}')
-                self._open()
                 return
 
-            noisily(f'Fetching database dump {mtime}, previously {last_mtime}')
-            for child in self.DB_DIR.iterdir():
-                if child.is_dir():
-                    shutil.rmtree(str(child))
+            noisily(f'Fetching and importing database dump at {mtime}, previous at {last_mtime}')
+            cls.DB_PATH.unlink(missing_ok=True)
+            db = CratesioDB(check=False, cache_mb=cache_mb)
+
+            csv.field_size_limit(2 ** 30) # There are large fields.
             with tarfile.open(fileobj=resp, mode='r|gz') as tar:
-                tar.extractall(path=self.DB_DIR)
+                for member in tar:
+                    name = member.path.split('/')[-1]
+                    if name in cls.INSERTERS:
+                        sql, preprocessor = cls.INSERTERS[name]
+                        fileobj = tar.extractfile(member)
+                        assert fileobj is not None
+                        rdr = csv.DictReader(map(bytes.decode, fileobj))
+                        db.executemany(sql, map(preprocessor, rdr))
 
-        noisily('Importing data')
-        self.DB_PATH.unlink(missing_ok=True)
+            noisily(f'Creating indices')
+            db.executescript(cls.INIT_INDEX_SQL)
+            db.commit()
+            db.close()
 
-        self._open()
-        self.executescript(self.INIT_SQL)
-        csv.field_size_limit(2 ** 30) # There are large fields.
-        for tbl_name, insert_sql in self.INSERT_SQLS.items():
-            files = list(self.DB_DIR.glob(f'*/data/{tbl_name}.csv'))
-            assert len(files) == 1
-            with open(files[0], 'r') as fin:
-                rdr = csv.reader(fin, lineterminator='\n')
-                next(rdr) # Skip the header.
-                self.executemany(insert_sql, rdr)
+            cls.MTIME_PATH.write_text(mtime)
+            noisily('Database initialized')
 
-        self.commit()
-        self.MTIME_PATH.write_text(mtime)
-        noisily('Database initialized')
+def update_popular_crates(db: CratesioDB, *, time: str, limit: int) -> None:
+    class VersMajor(tuple):
+        RE_SEMVER = re.compile(r'^(\d+)\.(\d+)\.(\d+)(?:[-+].*)?$')
 
-def update_popular_crates(db: CratesioDB, *, time: str, limit: Optional[int] = None) -> None:
-    class Vers(object):
-        def __init__(self, s: str) -> None:
-            self.maj, self.min, self.pat = map(int, s.split('.'))
+        def __new__(cls: type[Self], s: str) -> Self:
+            m = cls.RE_SEMVER.match(s)
+            assert m is not None, f'Invalid semver: {s}'
+            maj, min, pat = map(int, (m[1], m[2], m[3]))
+            vers = (maj,) if maj else (maj, min) if min else (maj, min, pat)
+            return super().__new__(cls, vers)
 
         def __str__(self) -> str:
-            return f'{self.maj}.{self.min}.{self.pat}'
-
-        def compatible_with(self, rhs) -> bool:
-            return self.maj == rhs.maj if self.maj != 0 else self.min == rhs.min if self.min != 0 else self.pat != self.pat
+            return '.'.join(map(str, self))
 
         # Reversed.
-        def __lt__(self, rhs) -> bool:
-            return (self.maj, self.min, self.pat) > (rhs.maj, rhs.min, rhs.pat)
+        def __lt__(self, rhs: Self) -> bool:
+            return super().__gt__(rhs)
 
     noisily('Querying database')
     cursor = db.cursor()
@@ -151,36 +179,39 @@ def update_popular_crates(db: CratesioDB, *, time: str, limit: Optional[int] = N
         WHERE version_downloads.date > DATE('now', ?)
         GROUP BY version_id
         ORDER BY SUM(version_downloads.downloads) DESC
-        LIMIT ?
-    ''', [time, limit if limit is not None else -1])
-    crates = [(str(row[0]), Vers(row[1])) for row in cursor]
-    crates.sort()
+    ''', (time,))
+
+    crates_set: set[tuple[str, VersMajor]] = set()
+    for row in cursor:
+        crates_set.add((str(row[0]), VersMajor(row[1])))
+        if len(crates_set) == limit:
+            break
+
+    crates = sorted(crates_set)
 
     noisily('======== Start of top crates')
     for name, vers in crates:
         noisily(name, vers)
     noisily('======== End of top crates')
 
-    deps: list[tuple[str, list[Vers]]] = []
-    for name, vers in crates:
-        if deps and deps[-1][0] == name:
-            if vers.compatible_with(deps[-1][1][-1]):
-                continue
-            deps[-1][1].append(vers)
-        else:
-            deps.append((name, [vers]))
-
     out_path = POPULAR_CRATES_MANIFEST_PATH
     tmp_path = out_path.with_suffix('.tmp')
     with out_path.open('r') as fin, tmp_path.open('w') as fout:
         for line in fin:
-            fout.write(line + '\n')
-            if line == '[dependencies]':
+            fout.write(line)
+            if line.startswith('[dependencies]'):
                 break
-        for name, verss in deps:
-            fout.write(f'{name} = "{verss[0]}"\n')
-            for i, v in enumerate(verss[1:], 2):
-                fout.write(f'{name}-{i} = {{ package = "{name}", version = "{v}" }}\n')
+
+        last_name: Optional[str] = None
+        crate_idx = 1
+        for name, vers in crates:
+            if last_name != name:
+                crate_idx = 1
+                fout.write(f'{name} = "{vers}"\n')
+            else:
+                crate_idx += 1
+                fout.write(f'{name}-{crate_idx} = {{ package = "{name}", version = "{vers}" }}\n')
+            last_name = name
     tmp_path.replace(out_path)
 
     noisily('Updating lock file')
@@ -192,7 +223,7 @@ def update_popular_crates(db: CratesioDB, *, time: str, limit: Optional[int] = N
         stdout=subprocess.DEVNULL,
     )
 
-def print_popular_crates(db: CratesioDB, *, time: str, limit: Optional[int], pat: Optional[str]) -> None:
+def list_popular_crates(db: CratesioDB, *, time: str, pat: Optional[str]) -> None:
     noisily('Querying database')
     cursor = db.cursor()
     cursor.execute('''
@@ -203,8 +234,7 @@ def print_popular_crates(db: CratesioDB, *, time: str, limit: Optional[int], pat
         WHERE crates.name LIKE ? AND version_downloads.date > DATE('now', ?)
         GROUP BY versions.crate_id
         ORDER BY crate_downloads DESC
-        LIMIT ?
-    ''', [pat if pat is not None else '%', time, limit if limit is not None else -1])
+    ''', (pat if pat is not None else '%', time))
     try:
         for row in cursor:
             print(row[0], row[1])
@@ -213,7 +243,7 @@ def print_popular_crates(db: CratesioDB, *, time: str, limit: Optional[int], pat
         exit(1)
 
 def update_proc_macro_crates(db: CratesioDB, *, concurrency: int) -> None:
-    TMP_CRATES_TOML_DIR.mkdir(exist_ok=True)
+    CRATES_TOML_DIR.mkdir(exist_ok=True)
 
     noisily('Querying database')
     cursor = db.cursor()
@@ -221,9 +251,9 @@ def update_proc_macro_crates(db: CratesioDB, *, concurrency: int) -> None:
         SELECT crates.name, versions.num AS max_vers, MAX(versions.created_at)
         FROM versions
         JOIN crates ON crates.id == crate_id
-        WHERE yanked == 'f'
+        WHERE NOT yanked
         GROUP BY crate_id
-        ORDER BY crates.name asc
+        ORDER BY crates.name ASC
     ''')
 
     RE_ITEMS = re.compile(r'"([^"]*)"', re.S)
@@ -231,7 +261,7 @@ def update_proc_macro_crates(db: CratesioDB, *, concurrency: int) -> None:
 
     async def load_or_fetch(sema: asyncio.Semaphore, name: str, version: str) -> None:
         try:
-            out_path = TMP_CRATES_TOML_DIR / f'{name}.toml'
+            out_path = CRATES_TOML_DIR / f'{name}.toml'
             if not out_path.exists():
                 noisily(f'GET {name} {version}')
                 url = CRATE_TARBALL_URL.format(name=name, version=version)
@@ -293,31 +323,32 @@ def update_proc_macro_crates(db: CratesioDB, *, concurrency: int) -> None:
 
 def main() -> None:
     parser = argparse.ArgumentParser(description='Metadata updater and utilities using crates.io database dump')
-    parser.add_argument('--sync', action='store_true', help='Synchronizing the database even if it exists')
+    parser.add_argument('--cache-mb', type=int, required=False, default=1024, help='Sqlite cache size in MiB')
     subparser = parser.add_subparsers(required=True)
 
-    p = subparser.add_parser('update-popular-crates')
-    p.add_argument('--limit', type=int, required=False, default=None, help='Number of top crates')
+    p = subparser.add_parser('sync', help='Synchronize or initialize the database')
+    p.set_defaults(sync=True)
+
+    p = subparser.add_parser('update-popular-crates', help='Update popular crates cache')
+    p.add_argument('--limit', type=int, required=False, default=256, help='Number of top crates to cache')
     p.add_argument('--time', type=str, default='-90 days', help='Time period to count recent downloads')
     p.set_defaults(fn=update_popular_crates)
 
-    p = subparser.add_parser('print-popular-crates')
-    p.add_argument('--limit', type=int, required=False, help='Number of top crates')
+    p = subparser.add_parser('list-popular-crates', help='Print popular crates with download counts in a given period')
     p.add_argument('--time', type=str, default='-90 days', help='Time period to count recent downloads')
     p.add_argument('--pat', type=str, default=None, help='Crate name pattern to filter for SQL "LIKE"')
-    p.set_defaults(fn=print_popular_crates)
+    p.set_defaults(fn=list_popular_crates)
 
-    p = subparser.add_parser('update-proc-macro-crates')
+    p = subparser.add_parser('update-proc-macro-crates', help='Update the list of proc-macro crates')
     p.add_argument('--concurrency', type=int, default=16, help='Connection concurrency')
     p.set_defaults(fn=update_proc_macro_crates)
 
     args = parser.parse_args()
-    fn, sync = args.fn, args.sync
-
-    subargs = vars(args)
-    del subargs['sync'], subargs['fn']
-
-    with CratesioDB(sync=sync) as db:
-        fn(db, **subargs)
+    if 'sync' in args:
+        CratesioDB.sync(cache_mb=args.cache_mb)
+    else:
+        subargs = vars(args)
+        with CratesioDB(cache_mb=subargs.pop('cache_mb')) as db:
+            subargs.pop('fn')(db, **subargs)
 
 main()
