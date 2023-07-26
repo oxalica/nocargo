21556646b5377cb9ad364dcea78c6c7cb09eea71
Rígille S. B. Menezes <rigillesbmenezes@gmail.com>
2023-07-26 15:54:59 -0300

    now really ignore registry layout

--- lib/pkg-info.nix
+++ lib/pkg-info.nix
@@ -111,9 +111,9 @@ rec {
     mkSrc:
     # https://github.com/rust-lang/cargo/blob/2f3df16921deb34a92700f4d5a7ecfb424739558/src/cargo/sources/registry/mod.rs#L259
     { name, vers, deps, features, cksum, yanked ? false, links ? null, v ? 1, ... }:
-    if v != 1 then
-      throw "${name} ${vers}: Registry layout version ${toString v} is too new to understand"
-    else
+    #if v != 1 then
+      #throw "${name} ${vers}: Registry layout version ${toString v} is too new to understand"
+    #else
     {
       inherit name features yanked links;
       version = vers;
